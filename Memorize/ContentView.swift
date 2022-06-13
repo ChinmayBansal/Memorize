a0cbbf24046b849c2ae607351cfa476936f12367
Chinmay Bansal <chinmaybansalsc@outlook.com>
2022-06-13 14:46:01 -0700

    Added simple button, ZStack

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,10 +9,68 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack(alignment: .center ){
+            RoundedRectangle(cornerRadius: 20.0)
+                    .stroke(lineWidth: 3.0)
+            Text("Hello, World!")
+                  
+               
+        }
+        .padding(.horizontal)
+        .foregroundColor(.green)
+//        return RoundedRectangle(cornerRadius: 20.0)
+//            .stroke(lineWidth: 3.0)
+//            .padding(.horizontal)
+//            .foregroundColor(/*@START_MENU_TOKEN@*/.green/*@END_MENU_TOKEN@*/)
+//        Text("Hello, World!").
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
